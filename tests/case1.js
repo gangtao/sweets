ce94b6fdef1a084afd7259ee8dd48748c8bfe77b
gangtao <gang.tao@outlook.com>
2020-11-09 19:44:10 -0800

    update test case

--- tests/case1.js
+++ tests/case1.js
@@ -7,23 +7,24 @@ var failureRate1 = new Rate("check_failure_rate1");
 var failureRate2 = new Rate("check_failure_rate2");
 
 export default function() {
-    let response1 = http.post("http://localhost:8080/sweets/v1/cs/configs?dataId=mydata&group=mygroup&content=hello");
+    const dataId = "mydata"
+    const group = "mygroup"
+    const content = "hello"
+
+    let response1 = http.post(`http://localhost:8080/sweets/v1/cs/configs?dataId=${dataId}&group=${group}&content=${content}`);
     // check() returns false if any of the specified conditions fail
     let checkRes1 = check(response1, {
         "status is 200": (r) => r.status === 200
     });
-
     // We reverse the check() result since we want to count the failures
     failureRate1.add(!checkRes1);
 
-    let response2 = http.get("http://localhost:8080/sweets/v1/cs/configs?dataId=mydata&group=mygroup");
-
+    let response2 = http.get(`http://localhost:8080/sweets/v1/cs/configs?dataId=${dataId}&group=${group}`);
     // check() returns false if any of the specified conditions fail
     let checkRes2 = check(response2, {
         "status is 200": (r) => r.status === 200,
-        "content is present": (r) => r.body.indexOf("hello") !== -1,
+        "content is present": (r) => r.body.indexOf(content) !== -1,
     });
-
     // We reverse the check() result since we want to count the failures
     failureRate2.add(!checkRes2);
 };
